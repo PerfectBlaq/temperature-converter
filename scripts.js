99b5383f440e9073d86d67898f102124ef125f16
MGBECHETA TIAGO <udochukwutiago84@gmail.com>
2026-07-06 00:38:33 +0100

    Add temperature conversion elements and selectors in scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const tempValue = document.getElementById("entered_values");
+const fromTemp = document.getElementById("from_temperature");
+const toTemp = document.getElementById("to_temperature");
+const fromTempDescr = document.querySelector(".from_unit_description");
+const toTempDescr = document.querySelector(".to_unit_descripion");
+const convertBtn = document.querySelector(".conversion_button");
+// console.log(fromTempDescr, toTempDescr);
