78b5b5dc8116795f939530295b6629477e9d4726
MGBECHETA TIAGO <udochukwutiago84@gmail.com>
2026-07-08 00:01:34 +0100

    Fix output value selector and add conversion logic for temperature calculations

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ const toTempDescr = document.querySelector(".to_unit_descripion");
 const convertBtn = document.querySelector(".conversion_button");
 /* Selected elements from the DOM that has to do with the temperature result */
 const inputValue = document.querySelector(".input_result");
-const outputValue = document.querySelector("output_result");
+const outputValue = document.querySelector(".output_result");
 /* Temperatures and their description C - celcius, k-kelvin... */
 const cDescr = "Metric scale where water freezes at 0°C and boils at 100°C";
 const fDescr = "Imperial scale where water freezes at 32°F and boils at 212°F";
@@ -34,7 +34,7 @@ function state(value) {
     fromTemp = temp;
   } else if (iD === "to_temperature") {
     toTemp = temp;
-  }else{
+  } else {
     return;
   }
 }
@@ -50,6 +50,49 @@ toSelect.addEventListener("change", (info) => {
   console.log(fromTemp, toTemp);
 });
 /* Conversion formula functions */
-// function farenheit(
-
-// )
+const converter = {
+  // Farenheit to celcius
+  fhrCel: function (F) {
+    return ((F - 32) * 5) / 9;
+  },
+  // Farenheit to kelvin
+  fhrKel: function (F) {
+    return ((F - 32) * 5) / 9 + 273.15;
+  },
+  // Celcius to Farenheit
+  celFhr: function (C) {
+    return (C * 9) / 5 + 32;
+  },
+  // Celcius to Kelvin
+  celKel: function (C) {
+    return C + 273.15;
+  },
+  // Kelvin to fahrenheit
+  kelFhr: function (K) {
+    return ((K - 273.15) * 9) / 5 + 32;
+  },
+  // Kelvin to celcius
+  kelCel: function (K) {
+    return K - 273.15;
+  },
+};
+convertBtn.addEventListener("click", () => {
+  let valueToConvert = tempValue.valueAsNumber;
+  let convertedValue;
+  console.log(valueToConvert);
+  if (fromTemp === "Farenheit" && toTemp === "Celsius") {
+    convertedValue = converter.fhrCel(valueToConvert);
+  } else if (fromTemp === "Farenheit" && toTemp === "Kelvin") {
+    convertedValue = converter.fhrKel(valueToConvert);
+  } else if (fromTemp === "Celcius" && toTemp === "Farenheit") {
+    convertedValue = converter.celFhr(valueToConvert);
+  } else if (fromTemp === "Celsius" && toTemp === "Kelvin") {
+    convertedValue = converter.celKel(valueToConvert);
+  } else if (fromTemp === "Kelvin" && toTemp === "Farenheit") {
+    convertedValue = converter.kelFhr(valueToConvert);
+  } else if (fromTemp === "Kelvin" && toTemp === "Celsius") {
+    convertedValue = converter.kelCel(valueToConvert);
+  }
+  inputValue.textContent = `${valueToConvert}`;
+  outputValue.textContent = `${convertedValue}`;
+});
